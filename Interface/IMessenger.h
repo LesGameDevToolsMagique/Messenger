a09ce544e93e4f2b40b59f687f2a464213fc0932
Sparkh <sp4rkh@MacBook-Pro-de-Jean-Antoine.local>
2016-03-11 16:45:09 +0800

    add code file
    Add main.cpp file (just main function in it)
    Add Messenger interface (interface for communication function)

---
+++ Interface/IMessenger.h
@@ -0,0 +1,26 @@
+//
+// Created by Jean-Antoine Dupont on 11/03/2016.
+//
+
+#ifndef     MESSENGER_IMESSENGER_H
+#define     MESSENGER_IMESSENGER_H
+
+#include    <sys/socket.h>
+#include    <sys/types.h>
+
+class               IMessenger
+{
+public:
+    virtual ~IMessenger() {};
+
+    virtual int     sendMessage(const int sock_fd,
+                                const void *msg, const unsigned int msg_size,
+                                const unsigned int flags = 0,
+                                const struct sockaddr *to = nullptr) = 0;
+    virtual void    *receiveMessage(const int sock_fd,
+                                    const unsigned int read_size = 4096,
+                                    const unsigned int flags = 0,
+                                    struct sockaddr *from = nullptr, socklen_t *from_size = 0) = 0;
+};
+
+#endif  /*  MESSENGER_IMESSENGER_H  */
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Jean-Antoine Dupont on 11/03/2016.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
